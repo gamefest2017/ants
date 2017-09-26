419c34e884cff7ff04b3b0ff4bb31368be7d2d8a
user <user@hudecova.bratislava-apollo.sk.ibm.com>
2017-09-26 14:33:44 +0200

    Move of the ant home after finding food, some Direction basic methods.

--- Ants/src/com/ibm/sk/dto/enums/Direction.java
+++ Ants/src/com/ibm/sk/dto/enums/Direction.java
@@ -2,6 +2,8 @@ package com.ibm.sk.dto.enums;
 
 import java.awt.Point;
 
+import javax.swing.SwingWorker;
+
 public enum Direction {
 	NORD(new Point(0,1)),
 	NORDEAST(new Point(-1,1)),
@@ -19,5 +21,70 @@ public enum Direction {
 	}
 	public Point getPositionChange() {
 		return this.point;
+	}
+	
+	
+	public static Direction addHorizontalAndVerticalD(Direction horizontalD, Direction verticalD) {
+		Direction returnValue = Direction.NO_MOVE;
+		if (horizontalD == Direction.NO_MOVE) {			
+			returnValue = verticalD;
+		} else if (horizontalD == Direction.WEST) {
+			switch (verticalD) {
+			case NORD: 		returnValue = Direction.NORDWEST;
+							break;
+			case NO_MOVE: 	returnValue = Direction.WEST;
+							break;
+			case SOUTH: 	returnValue = Direction.SOUTHWEST;
+							break;
+			default:
+				break;				
+			}			
+		} else {
+			switch (verticalD) {
+			case NORD: 		returnValue = Direction.NORDEAST;
+							break;
+			case NO_MOVE: 	returnValue = Direction.EAST;
+							break;
+			case SOUTH: 	returnValue = Direction.SOUTHEAST;
+							break;
+			default:
+				break;				
+			}
+		}
+		return returnValue;
+	}
+	
+	public static Direction add(Direction d1, Direction d2) {
+		Direction returnValue = Direction.NO_MOVE;
+		int x = d1.point.x + d2.point.x;
+		int y = d1.point.y + d2.point.y;
+		
+		if (x == 0) {
+			switch (y) {
+			case -1: case -2: returnValue = Direction.SOUTH;
+				break;
+			case 0: returnValue = Direction.NO_MOVE;
+				break;
+			case 1: case 2: returnValue = Direction.NORD;	
+			}
+		} else if (x<0) {
+			switch (y) {
+			case -1: case -2: returnValue = Direction.SOUTHWEST;
+				break;
+			case 0: returnValue = Direction.WEST;
+				break;
+			case 1: case 2: returnValue = Direction.NORDWEST;	
+			}
+		} else {
+			switch (y) {
+			case -1: case -2: returnValue = Direction.SOUTHEAST;
+				break;
+			case 0: returnValue = Direction.EAST;
+				break;
+			case 1: case 2: returnValue = Direction.NORDEAST;	
+			}
+		}
+		
+		return returnValue;
 	}
 }
