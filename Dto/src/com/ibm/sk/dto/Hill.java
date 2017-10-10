5a756747e777c0599a660bb96f455df44c2e6653
Peter Prazenica <peter.prazenica@sk.ibm.com>
2017-10-10 10:44:51 +0200

    Refactoring - DTOs should be shared between GUI and Engine

---
+++ Dto/src/com/ibm/sk/dto/Hill.java
@@ -0,0 +1,54 @@
+package com.ibm.sk.dto;
+
+
+import java.awt.Point;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Hill extends WorldObject {
+
+	private int food = 0;
+	private String name;
+	private final List<IAnt> ants;
+
+	public Hill(final int population, final double populationWarFactor, final String name, final Point position) {
+		this.name = name;
+		this.position = position;
+		this.ants = new CopyOnWriteArrayList<>();
+	}
+
+	public boolean isMyHill(final Ant ant) {
+		return ant != null && this.name.equals(ant.getMyHillName());
+	}
+
+	public int getPopulation() {
+		return this.ants.size();
+	}
+
+	public void setFood(final int foodAmount) {
+		this.food = foodAmount;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public Collection<IAnt> getAnts() {
+		return this.ants;
+	}
+
+	@Override
+	public String toString() {
+		return "Hill [population=" + this.ants.size() + ", name=" + this.name + ", food=" + this.food + "]";
+	}
+
+	public int getFood() {
+		return this.food;
+	}
+
+}
---
+++ Dto/src/com/ibm/sk/dto/matchmaking/strategy/SingleElimination.java
@@ -0,0 +1,36 @@
+package com.ibm.sk.dto.matchmaking.strategy;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.ibm.sk.dto.matchmaking.Match;
+import com.ibm.sk.dto.matchmaking.Player;
+import com.ibm.sk.dto.matchmaking.PlayerStatus;
+
+public class SingleElimination implements MatchmakingStrategy {
+
+	@Override
+	public List<Match> initialize(List<Player> players) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Match> update(Match finishedMatch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Player> getWinner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<PlayerStatus> getRanking() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
