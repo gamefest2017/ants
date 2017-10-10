bed899a3d22316d578bd8706a0475abcd6e989e5
Peter Prazenica <peter.prazenica@sk.ibm.com>
2017-10-10 07:23:33 +0200

    AH009 GUI for Tournament - Qualification

--- FallFestGUICommon/src/com/ibm/sk/ff/gui/common/objects/operations/InitMenuData.java
+++ FallFestGUICommon/src/com/ibm/sk/ff/gui/common/objects/operations/InitMenuData.java
@@ -2,12 +2,35 @@ package com.ibm.sk.ff.gui.common.objects.operations;
 
 import java.util.Arrays;
 
+import com.ibm.sk.dto.matchmaking.Tournament;
+import com.ibm.sk.dto.qualification.Qualification;
+
 public class InitMenuData {
 	
 	public String[] competitors = {};
 	
 	public String[] replays = {};
 	
+	public Qualification qualification;
+	
+	public Tournament tournament;
+
+	public Qualification getQualification() {
+		return qualification;
+	}
+
+	public void setQualification(Qualification qualification) {
+		this.qualification = qualification;
+	}
+
+	public Tournament getTournament() {
+		return tournament;
+	}
+
+	public void setTournament(Tournament tournament) {
+		this.tournament = tournament;
+	}
+
 	public void addCompetitor(String competitor) {
 		competitors = Arrays.copyOf(competitors, competitors.length + 1);
 		competitors[competitors.length - 1] = competitor;
